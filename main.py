21117e459e66ca594e2466c63693a0db9535ec63
Kiakkers <kiakkers.zzy@gmail.com>
2026-01-07 15:09:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ if gen:
   st.image(f"lizard{num}.jpg", caption="this is a lizard.")
 
 
-name = st.chat_input(placeholder="ask about lizards here", *, key=None, max_chars=None, accept_file=False, file_type=None, accept_audio=False, audio_sample_rate=16000, disabled=False, on_submit=None, args=None, kwargs=None, width="stretch")
+name = st.chat_input(placeholder="ask about lizards here")
 submitted = st.button('**Ask**')
 if submitted:
   st.write(get_answer(name,choice))
